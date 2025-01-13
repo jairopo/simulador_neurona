8efcbf9951bffc31ad5111af54e52c59c5ea602a
Jairo Andrades Bueno <jairo.po8@gmail.com>
2025-01-13 10:14:08 +0100

    cambiado a h3

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.header("Simulador de neurona")
 num = st.slider("Elige el número de entradas/pesos que tendrá la neurona", 1, 10)
 
 # Crea tantas columnas como pesos se hayan seleccionado
-st.markdown("<h1>Pesos</h1>", unsafe_allow_html=True)
+st.markdown("<h3>Pesos</h3>", unsafe_allow_html=True)
 col_w = st.columns(num)
 w = np.zeros(num)
 
@@ -21,7 +21,7 @@ for i in range(num):
 st.write(f"w = {w}")
 
 # Crea tantas columnas como entradas se hayan seleccionado
-st.markdown("<h1>Entradas>/h1>", unsafe_allow_html=True)
+st.markdown("<h3>Entradas></h3>", unsafe_allow_html=True)
 col_x = st.columns(num)
 x = np.zeros(num)
 
@@ -32,9 +32,9 @@ for i in range(num):
 st.write(f"x = {x}")
 
 col_b_f = st.columns(2)
-col_b_f[0].markdown("<h1>Sesgo</h1>", unsafe_allow_html=True)
+col_b_f[0].markdown("<h3>Sesgo</h3>", unsafe_allow_html=True)
 b = col_b_f[0].number_input("Introduce el valor del sesgo")
-col_b_f[1].markdown("<h1>Función de activación</h1>", unsafe_allow_html=True)
+col_b_f[1].markdown("<h3>Función de activación</h3>", unsafe_allow_html=True)
 f = col_b_f[1].selectbox("Elige la función de activación", ["Sigmoide", "ReLu", "Tangente hiperbólica"])
 
 # Crea una instancia de la clase Neuron y calcula el resultado
